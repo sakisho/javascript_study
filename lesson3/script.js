18c5bb620db8fb6374eba3da0dac517e0d003717
Kazuya Takami <foundationmeister@outlook.com>
2021-04-22 22:24:10 +0900

    commit.

---
+++ lesson3/script.js
@@ -0,0 +1,14 @@
+document.getElementById("img1").addEventListener("click", () => {
+  let imgSrc = document.getElementById("img1").src;
+  document.getElementById("main").src = imgSrc;
+});
+
+document.getElementById("img2").addEventListener("click", () => {
+  let imgSrc = document.getElementById("img2").src;
+  document.getElementById("main").src = imgSrc;
+});
+
+document.getElementById("img3").addEventListener("click", () => {
+  let imgSrc = document.getElementById("img3").src;
+  document.getElementById("main").src = imgSrc;
+});
