daff38288196c8333e278ae699de5220df80c65e
Kazuya Takami <foundationmeister@outlook.com>
2021-06-12 09:40:46 +0900

    2021.06.08 commit.

--- lesson07/Demo-Corporate-js/js/form.js
+++ lesson07/Demo-Corporate-js/js/form.js
@@ -1,10 +1,33 @@
 $(function (){
   "use strict";
 
+  let
+    errorFlag,
+    formName  = $("[name='name']"),
+    formKana  = $("[name='kana']"),
+    formEmail = $("[name='email']"),
+    formPort  = $("[name='portfolio']"),
+    formArray = [formName, formKana, formEmail, formPort];
+
   $("#form").on("submit", function (){
-    if ($("[name='name']").val() === "") {
-      console.log("名前を入力してね");
-      return false;
-    }
+    errorFlag = true;
+
+    $.each(formArray, function(index, value) {
+      if (value.val() === "") {
+        value.next().show();
+        value.css("background", "#f0d0d0");
+        errorFlag = false;
+      }
+    });
+    return errorFlag;
+  });
+
+  $.each(formArray, function(index, value) {
+    value.on("input", function (){
+      if ($(this).val() !== "") {
+        $(this).css("background", "#fff");
+        $(this).next().hide();
+      }
+    });
   });
 });
