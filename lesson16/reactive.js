968a03e73ca90698fee7fd2e71704db1243bf5c9
Kazuya Takami <foundationmeister@outlook.com>
2021-08-04 08:46:59 +0900

    2021.08.03 commit.

---
+++ lesson16/reactive.js
@@ -0,0 +1,12 @@
+let app = new Vue({
+  el: "#app",
+  data: {
+    current: new Date()
+  },
+  created: function() {
+    let that = this;
+    this.timer = setInterval(function(){
+      that.current = new Date();
+    }, 1000);
+  }
+});
